4d0866068d4080ad4273a1d6c9218acc5b8cfa10
LeeDamson <damsoncolin@gmail.com>
2023-04-04 15:31:04 -0700

    Task 1-2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,11 +17,6 @@ class Rectangle:
     def width(self):
         return self.__width
 
-        """property def height(self): to retrieve it"""
-    @property
-    def height(self):
-        return self.__height
-
         """property setter def width(self, value): to set it"""
     @width.setter
     def width(self, value):
@@ -30,12 +25,17 @@ class Rectangle:
             raise TypeError("width must be an integer")
         """if width is less than 0, raise a ValueError exception with the message width must be >= 0"""
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError(" width must be >= 0")
         self.__width = value
+
+        """property def height(self): to retrieve it"""
+    @property
+    def height(self):
+        return self.__height
 
         """property setter def height(self, value): to set it"""
     @height.setter
-    def width(self, value):
+    def height(self, value):
         """height must be an integer, otherwise raise a TypeError exception with the message height must be an integer"""
         if not isintstance(value, int):
             raise TypeError("height must be an integer")
